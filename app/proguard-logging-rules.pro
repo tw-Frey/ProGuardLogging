e420b62b72327b35157cf4721331f322b093b765
FY <tw-Frey@users.noreply.github.com>
2021-10-13 17:12:10 +0800

    With the option -assumevalues, fields and methods with primitive return types can have values or ranges of values.
    Signed-off-by: FY <tw-Frey@users.noreply.github.com>

--- app/proguard-logging-rules.pro
+++ app/proguard-logging-rules.pro
@@ -1,5 +1,6 @@
 # no logging in production
 # https://medium.com/yazio-engineering/6566bc387b63
+# 不要把 isLoggable 去邊際化, 它是有用的, 應該要用 -assumevalues 植入期待植 (旁白 by FY)
 -assumenosideeffects class android.util.Log {
   v(...);
   d(...);
@@ -9,4 +10,25 @@
   wtf(...);
   println(...);
 }
-# 不要把 isLoggable 去邊際化, 它是有用的, 應該要用 -assumevalues 植入期待植 (旁白 by FY)
+###################################################################################################
+# https://www.guardsquare.com/manual/configuration/usage
+# With the option -assumevalues, fields and methods with primitive return types can have
+# values or ranges of values. The assignment keyword is = or return, interchangeably.
+# For example, "boolean flag = true;" or "int method() return 5;". Ranges of values are
+# separated by .., for example, "int f = 100..200;". A range includes its begin value and end value.
+###################################################################################################
+# 不受 debuggable 影響, 但受 optimiz 影響
+-assumevalues class android.util.Log {
+    boolean isLoggable(...) return false;
+}
+###################################################################################################
+# 0,1 非有效值
+# VERBOSE = 2
+# DEBUG = 3
+# INFO = 4
+# WARN = 5
+# ERROR = 6
+# ASSERT = 7
+###################################################################################################
+# 不受 optimiz 影響, 但受 debuggable 影響
+-maximumremovedandroidloglevel 8
--- app/src/main/java/tw/idv/fy/proguard/logging/MainActivity.kt
+++ app/src/main/java/tw/idv/fy/proguard/logging/MainActivity.kt
@@ -2,19 +2,31 @@ package tw.idv.fy.proguard.logging
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.*
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tv = findViewById<View>(R.id.tv)
-        tv.postDelayed(object: Runnable{
-            var count = 0
-            override fun run() {
-                android.util.Log.i("Faty", "${++count}")
-                tv.postDelayed(this, 500L)
-            }
-        }, 500L)
+//        val tv = findViewById<View>(R.id.tv)
+//        tv.postDelayed(object: Runnable{
+//            var count = 0
+//            override fun run() {
+//                android.util.Log.i("Faty", "${++count}")
+//                tv.postDelayed(this, 500L)
+//            }
+//        }, 500L)
+        val tv = findViewById<android.widget.TextView>(R.id.tv)
+        tv.text = "BUILD_TYPE = ${BuildConfig.BUILD_TYPE}\nDebuggable = ${BuildConfig.DEBUG}\n" +
+                "0 = ${isLoggable("Faty", 0)}\n" +
+                "1 = ${isLoggable("Faty", 1)}\n" +
+                "VERBOSE = ${isLoggable("Faty", VERBOSE)}\n" +
+                "DEBUG = ${isLoggable("Faty", DEBUG)}\n" +
+                "INFO = ${isLoggable("Faty", INFO)}\n" +
+                "WARN = ${isLoggable("Faty", WARN)}\n" +
+                "ERROR = ${isLoggable("Faty", ERROR)}\n" +
+                "ASSERT = ${isLoggable("Faty", ASSERT)}"
     }
 }
